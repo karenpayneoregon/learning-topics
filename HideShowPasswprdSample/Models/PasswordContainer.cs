5e1a6b27e00913b1cd342493eddf6bebc617ed27
Karen Payne <paynekaren@comcast.net>
2025-08-30 11:09:29 -0700

    Add password management features and Spectre.Console
    Updated project to include Spectre.Console package.
    Enhanced password input fields in Index.cshtml with model binding and added a submit button.
    Modified IndexModel to handle password data and logging.
    Introduced PasswordContainer class for better organization of password fields.
    
    by Karen

---
+++ HideShowPasswprdSample/Models/PasswordContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace HideShowPasswordSample.Models;
+
+public class PasswordContainer
+{
+    public  string? Password1 { get; set; }
+    public  string? Password2 { get; set; }
+    public  string? Password3 { get; set; }
+    public  string? Password4 { get; set; }
+
+}
--- HideShowPasswprdSample/Pages/Index.cshtml.cs
+++ HideShowPasswprdSample/Pages/Index.cshtml.cs
@@ -1,18 +1,41 @@
+using HideShowPasswordSample.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Spectre.Console;
+
+// ReSharper disable ConvertConstructorToMemberInitializers
 
 namespace HideShowPasswordSample.Pages;
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    [BindProperty]
+    public required PasswordContainer PasswordContainer { get; set; }
+
 
     public void OnGet()
     {
+        PasswordContainer = new PasswordContainer()
+        {
+            Password1 = "MySecretPassword1",
+            Password2 = "MySecretPassword2",
+            Password3 = "MySecretPassword3"
+        };
+
+       
+    }
+
+    public void OnPost()
+    {
+        AnsiConsole.MarkupLine(PasswordContainer.Password1 is not null
+            ? $"[yellow]Password 1:[/] {PasswordContainer.Password1}"
+            : "[red]Password 1 is null[/]");
+        AnsiConsole.MarkupLine(PasswordContainer.Password2 is not null
+            ? $"[yellow]Password 2:[/] {PasswordContainer.Password2}"
+            : "[red]Password 2 is null[/]");
+        AnsiConsole.MarkupLine(PasswordContainer.Password3 is not null
+            ? $"[yellow]Password 3:[/] {PasswordContainer.Password3}"
+            : "[red]Password 3 is null[/]");
 
     }
 }

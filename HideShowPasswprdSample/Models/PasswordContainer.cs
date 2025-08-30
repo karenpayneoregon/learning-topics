c3701bcebc87d933f4d92db08e67a28ec6f6091c
Karen Payne <paynekaren@comcast.net>
2025-08-30 15:30:03 -0700

    Remove Password4 from PasswordContainer class
    by Karen

--- HideShowPasswprdSample/Models/PasswordContainer.cs
+++ HideShowPasswprdSample/Models/PasswordContainer.cs
@@ -7,6 +7,4 @@ public class PasswordContainer
     public  string? Password1 { get; set; }
     public  string? Password2 { get; set; }
     public  string? Password3 { get; set; }
-    public  string? Password4 { get; set; }
-
 }

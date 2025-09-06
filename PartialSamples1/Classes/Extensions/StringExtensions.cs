ff6c0a0422ca13d4e3202f22f7b489d0665cc682
Karen Payne <paynekaren@comcast.net>
2025-09-06 03:01:13 -0700

    tweak

--- PartialSamples1/Classes/Extensions/StringExtensions.cs
+++ PartialSamples1/Classes/Extensions/StringExtensions.cs
@@ -6,10 +6,7 @@ public static class StringExtensions
     /// </summary>
     /// <param name="input">The string to capitalize.</param>
     /// <returns>A new string with the first letter capitalized. If the input is null or empty, the original string is returned.</returns>
-    public static string CapitalizeFirstLetter(this string input)
-    {
-        if (string.IsNullOrWhiteSpace(input)) return input;
-
-        return char.ToUpper(input[0]) + input.AsSpan(1).ToString();
-    }
+    public static string CapitalizeFirstLetter(this string input) 
+        => string.IsNullOrWhiteSpace(input) ? 
+            input : char.ToUpper(input[0]) + input.AsSpan(1).ToString();
 }

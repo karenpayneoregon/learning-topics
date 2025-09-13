fff820d86144abf937ee64c2dd10540a239904e8
Karen Payne <paynekaren@comcast.net>
2025-09-13 16:45:25 -0700

    Add ModelFormatCustomSample project and enhancements
    by Karen

---
+++ ModelFormatCustomSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ModelFormatCustomSample.Classes;
+using ModelFormatCustomSample.Models;
+using Spectre.Console;
+
+namespace ModelFormatCustomSample;
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        Customer customer = new(1, "Karen", "Payne", new DateOnly(1956, 9, 24));
+
+        AnsiConsole.MarkupLine($"[cyan]A[/]: {customer:A}");
+        AnsiConsole.MarkupLine($"[cyan]F[/]: {customer:F}");
+        AnsiConsole.MarkupLine($"[cyan]N[/]: {customer:N}");
+        AnsiConsole.MarkupLine($"[cyan]B[/]: {customer:B}");
+        AnsiConsole.MarkupLine($"[cyan]I[/]: {customer:I}");
+        AnsiConsole.MarkupLine($"[cyan]C[/]: {customer}");
+        AnsiConsole.MarkupLine($"[cyan]T[/]: {customer.ToString()}");
+
+        Console.WriteLine();
+
+        var (firstName, lastName, birthDate) = customer;
+        AnsiConsole.MarkupLine($"[cyan]Deconstruct[/]: {firstName} {lastName}, born on {birthDate}");
+        SpectreConsoleHelpers.ExitPrompt();
+    }
+}

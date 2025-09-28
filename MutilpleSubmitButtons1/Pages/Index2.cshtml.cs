52d55cce793394c65c834530227ae185d5811000
Karen Payne <paynekaren@comcast.net>
2025-09-28 15:34:31 -0700

    Refactor Index2.cshtml and Index2.cshtml.cs
    Removed unused `<script>` section from Index2.cshtml, which contained commented-out JavaScript for toggling debugger CSS.
    
    In Index2.cshtml.cs:
    - Removed redundant nested `Index2Model` class declaration.
    - Reformatted `OnGet` method and XML comments for clarity.
    
    by Karen

--- MutilpleSubmitButtons1/Pages/Index2.cshtml.cs
+++ MutilpleSubmitButtons1/Pages/Index2.cshtml.cs
@@ -1,24 +1,23 @@
 #nullable disable
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MultipleSubmitButtons1.Pages
-{
-    public class Index2Model : PageModel
-    {
-        public string ProgramParam { get; private set; }
-        public int? CountParam { get; private set; }
+namespace MultipleSubmitButtons1.Pages;
 
-        /// <summary>
-        /// Handles GET requests to the page from Index1.
-        /// </summary>
-        /// <remarks>
-        /// This method initializes the <see cref="ProgramParam"/> and <see cref="CountParam"/> properties
-        /// by retrieving their values from the session state.
-        /// </remarks>
-        public void OnGet()
-        {
-            ProgramParam = HttpContext.Session.GetString("Program");
-            CountParam = HttpContext.Session.GetInt32("Count");
-        }
+public class Index2Model : PageModel
+{
+    public string ProgramParam { get; private set; }
+    public int? CountParam { get; private set; }
+
+    /// <summary>
+    /// Handles GET requests to the page from Index1.
+    /// </summary>
+    /// <remarks>
+    /// This method initializes the <see cref="ProgramParam"/> and <see cref="CountParam"/> properties
+    /// by retrieving their values from the session state.
+    /// </remarks>
+    public void OnGet()
+    {
+        ProgramParam = HttpContext.Session.GetString("Program");
+        CountParam = HttpContext.Session.GetInt32("Count");
     }
 }

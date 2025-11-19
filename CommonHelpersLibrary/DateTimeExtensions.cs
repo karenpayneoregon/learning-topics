1d987aabf9f482ff2b65d52019e1648705bf17da
Karen Payne <paynekaren@comcast.net>
2025-11-19 07:09:36 -0800

    usings removed

--- CommonHelpersLibrary/DateTimeExtensions.cs
+++ CommonHelpersLibrary/DateTimeExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CommonHelpersLibrary;
+﻿namespace CommonHelpersLibrary;
 public static class DateTimeExtensions
 {
     public static (DateOnly saturday, DateOnly sunday) GetWeekendDates(this DateTime date, DayOfWeek startOfWeek = DayOfWeek.Sunday)

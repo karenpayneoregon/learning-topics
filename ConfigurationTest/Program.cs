593c8d3c25b67f6cebba0d9bb505520c18fc47aa
Karen Payne <paynekaren@comcast.net>
2026-05-07 14:39:27 -0700

    new code

--- ConfigurationTest/Program.cs
+++ ConfigurationTest/Program.cs
@@ -1,4 +1,8 @@
-﻿using ConsoleConfigurationLibrary.Classes;
+﻿#nullable enable
+using System.Management;
+using ConsoleConfigurationLibrary.Classes;
+// ReSharper disable PossibleInvalidCastExceptionInForeachLoop
+#pragma warning disable CA1416
 
 namespace ConfigurationTest;
 
@@ -11,6 +15,24 @@ internal partial class Program
         
         Console.WriteLine(AppConnections.Instance.MainConnection);
         Console.WriteLine(EntitySettings.Instance.CreateNew);
+
+        Console.WriteLine();
+        Console.WriteLine(ComputerInfo.GetManufacturer());
         Console.ReadLine();
+    }
+}
+
+public static class ComputerInfo
+{
+    public static string? GetManufacturer()
+    {
+        using var searcher = new ManagementObjectSearcher("SELECT Manufacturer FROM Win32_ComputerSystem");
+
+        foreach (ManagementObject obj in searcher.Get())
+        {
+            return obj["Manufacturer"]?.ToString();
+        }
+
+        return null;
     }
 }
